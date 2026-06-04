676632732b507c6c11bae6be190f8cdf3a7e3fcc
sebasalinasdesigner-cloud <sebasalinasdesigner@gmail.com>
2026-06-04 11:54:05 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import joblib
+import pandas as pd
+
+# Diseño de la página web
+st.set_page_config(page_title="App de Salud Mental", page_icon="🧠")
+st.title("🧠 Predictor de Adicción a Redes Sociales")
+st.write("¡Bienvenido/a! Esta es la interfaz visual de mi proyecto de Inteligencia Artificial de Talento Tech.")
+
+# Mensaje para confirmar que funciona
+st.success("¡La aplicación web funciona y el modelo de IA está listo para usarse!")
